2930eb575329fb41d2cefb65af7236f157b6ba50
Gaurav Choudhary <32640118+GauravChoudhary8411@users.noreply.github.com>
2019-03-22 12:47:00 +0700

    Add files via upload

--- priority.c
+++ priority.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
 #include<string.h>
 #include<stdlib.h>
 struct process
 {
-	int i,j,n,m1,n1,m2,n2,m3,n3,m4,n4;
+	int i,j,n,count;
 	int a[1000],b[1000],pr[1000];
-	int setp,pos,temp;
 }p;
 void getdata()
 {
-	printf("\t*********************************WELCOME TO PRIORITY SCHEDULING ALGORITHM********************************\n\n\n");
+	printf("**********************************WELCOME TO PREEMTIVE PRIORITY SCHEDULING ALGORITHM***********************************\n\n\n");
 	printf("Enter the number of processes you want : ");
 	scanf("%d",&p.n);
 	
@@ -28,7 +26,6 @@ void getdata()
 	printf("Enter the Priority for process %d : ",p.i);
 	scanf("%d",&p.pr[p.i]);
 	}
-	system("cls");
 }
 void display()
 	{
@@ -38,27 +35,25 @@ void display()
 	}
 }
 void priority(){
-	for(p.i=0;p.i<p.n;p.i++)
-    {
-        p.pos=p.i;
-        for(p.j=p.i+1;p.j<p.n;p.j++)
-        {
-            if(p.pr[p.j]<p.pr[p.pos])
-                p.pos=p.j;
-        }
- 
-        p.temp=p.pr[p.i];
-        p.pr[p.i]=p.pr[p.pos];
-        p.pr[p.pos]=p.temp;
- 
-        p.temp=p.b[p.i];
-        p.b[p.i]=p.b[p.pos];
-        p.b[p.pos]=p.temp;
- 
-        p.temp=p.i;
-        p.i=p.i;
-        p.pos=p.temp;
-    }
+	p.count=0;
+	printf("Enter the process number you want to execute : ");
+	scanf("%d",&p.i);
+	p.pr[p.i]==0;
+	if(p.pr[p.i]==0){
+	p.pr[p.i]=1;
+	printf("%d is executing \n",p.i);
+	printf("\nProcess Number\t\t\tBurst Time\t\t\tArrival Time\t\t\tPriority\n");
+	printf("    %d\t\t\t\t    %d\t\t\t\t     %d\t\t\t\t    %d\n",p.i,p.b[p.i],p.a[p.i],p.pr[p.i]);
+	p.count++;
+	}
+	printf("\n\n\nProcesses in the ready queue are \n\n");
+	for(p.i=1;p.i<=p.n;p.i++){
+	if(p.pr[p.i]!=p.count++){
+	p.pr[p.i]=2;
+	printf("\nProcess Number\t\t\tBurst Time\t\t\tArrival Time\t\t\tPriority\n");
+	printf("    %d\t\t\t\t    %d\t\t\t\t     %d\t\t\t\t    %d\n",p.i,p.b[p.i],p.a[p.i],p.pr[p.i]);	
+		}
+	}
 }
 int main()
 {
